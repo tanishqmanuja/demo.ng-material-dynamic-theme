0351d8e28e55134183a084932443dd0fa2f4d4d3
tanishqmanuja <tanishqmanuja@gmail.com>
2024-08-10 13:03:24 +0530

    feat: add @angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,11 @@ import {
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes), provideAnimationsAsync(),
   ],
 };
